dda3ccb79b782e5e551b92ff5913bac376697ac0
Elias Romero <102391069+eliasromero72@users.noreply.github.com>
2022-08-12 09:56:25 -0300

    Add files via upload

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int soma = 0;
+int i = 0;
+
+for(i = 1; i <= 1000; i++)
+    soma = soma + i;
+
+Console.WriteLine($"A soma de {i} números é de: {soma}");
